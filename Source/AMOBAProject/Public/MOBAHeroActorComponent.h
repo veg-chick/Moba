c83824acd38368cd963a7467f290283660d3ff5a
FWdarling <1853204@tongji.edu.cn>
2019-05-22 16:06:25 +0800

    repair a build debug

--- Source/AMOBAProject/Public/MOBAHeroActorComponent.h
+++ Source/AMOBAProject/Public/MOBAHeroActorComponent.h
@@ -6,20 +6,33 @@
 #include "MOBABaseActorComponent.h"
 #include "MOBAHeroActorComponent.generated.h"
 
-/**
- * 
- */
+UENUM()
+enum HeroState { alive, dead, silience, imprison, stun };
+
+struct HeroPackage {
+
+	int packageOne;
+	int packageTwo;
+	int packageThree;
+	int packageFour;
+	int packageFive;
+	int packageSix;
+
+};
+
+
 UCLASS()
 class AMOBAPROJECT_API UMOBAHeroActorComponent : public UMOBABaseActorComponent
 {
 	GENERATED_BODY()
-	
 
 public:
 
 	UMOBAHeroActorComponent();
 
+
 protected:
+
 	HeroState heroState;
 
 };
